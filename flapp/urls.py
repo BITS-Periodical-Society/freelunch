04364a62296f06fa6b7aed74a28d0a5e458a32fb
Akshay Mittal <46865292+Ak-shay@users.noreply.github.com>
2019-06-19 01:20:02 +0530

    django version changed to 2.2.2 (#37)
    * django version changed to 2.2.2
    
    * Added comments
    
    * Comments Added

--- flapp/urls.py
+++ flapp/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
 urlpatterns = [
 	path('', views.post_list,name='post_list'),
     path('', views.post_list, name='Home'),
-    path('Authors', views.Authors, name='Authors'),
-    path('post_detail', views.post_detail, name='post_detail'),
-    url(r'^about', views.about , name="about"),
-    url(r'^contributors', views.contributors, name = "contributors" ),
-    url(r'^contribute', views.contribute, name= "contribute" ),
-    url(r'^science-technology', views.sciencetechnology, name= "sciencetechnology" ),
-    url(r'^economics-finanace', views.economicsfinance, name= "economicsfinance" ),
-    url(r'^world-affairs', views.worldaffairs, name= "worldaffairs" ),
-    url(r'^editorial', views.editorial, name= "editorial" ),
-    url(r'^book-reviews', views.bookreviews, name= "book-reviews" ),
+    path('Authors/', views.Authors, name='Authors'),
+    path('post_detail/', views.post_detail, name='post_detail'),
+    # path('about/', views.about , name="about"),
+    # path('contributors/', views.contributors, name = "contributors" ),
+    # path('contribute/', views.contribute, name= "contribute" ),
+    # path('science-technology/', views.sciencetechnology, name= "sciencetechnology" ),
+    # path('economics-finanace/', views.economicsfinance, name= "economicsfinance" ),
+    # path('world-affairs/', views.worldaffairs, name= "worldaffairs" ),
+    # path('editorial/', views.editorial, name= "editorial" ),
+    # path('book-reviews/', views.bookreviews, name= "book-reviews" ),
 ]
