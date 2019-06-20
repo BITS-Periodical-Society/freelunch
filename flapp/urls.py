2859ce732ea88c15f30425082c9617160c9847dc
Ak-shay <mittalakshay2000@gmail.com>
2019-06-20 16:35:54 +0530

    function based views to Class Based Views fixes #51

--- flapp/urls.py
+++ flapp/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.post_list, name='post_list'),
-    path('', views.post_list, name='Home'),
-    path('Authors/', views.Authors, name='Authors'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path('category/<slug:slug>/', views.post_list, name="category_post" ),
+	path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.PostListView.as_view(), name='Home'),
+    path('Authors/', views.AuthorListView.as_view(), name='Authors'),
+    path('<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('category/<slug:slug>/', views.PostListView.as_view(), name="category_post" ),
     # path('about/', views.about , name="about"),
     # path('contributors/', views.contributors, name = "contributors" ),
 ]
--- flapp/views.py
+++ flapp/views.py
@@ -1,43 +1,39 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
 from django.utils import timezone
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView
 from .models import Post, Category
 from .forms import PostForm
 
 
+class PostListView(ListView):
+	"""
+	Returns posts with category filter.
+	"""
+	context_object_name = 'posts'
+	template_name = 'blog/post_list.html'
+	paginate_by = 2
 
-def index(request):
-	return render(request,"index.html")
+	def get_queryset(self):
+		try:
+			slug = self.kwargs['slug']
+			category = Category.objects.get(slug=slug)
+			posts = category.post_set.all().order_by('-published_date')
+		except:
+			posts = Post.objects.all()
+		return posts
 
-def post_list(request, **kwargs):
-	try:
-		slug = kwargs['slug']
-		category = Category.objects.get(slug=slug)
-		posts = category.post_set.all().order_by('-published_date')
-	except:
-		posts = Post.objects.all()
-	paginator = Paginator(posts, 1)
-	page_request_var = 'page'
-	page = request.GET.get(page_request_var)
 
-	try:
-		paginated_queryset = paginator.page(page)
-	except PageNotAnInteger:
-		paginated_queryset = paginator.page(1)
-	except EmptyPage:
-		paginated_queryset = paginator.page(paginator.num_pages)
+class PostDetailView(DetailView):
+	"""
+	Detail view for a post.
+	"""
+	model = Post
+	context_object_name = 'post'
+	template_name = 'blog/post_detail.html'
+	slug_url_kwarg = 'slug'
 
-	context = {
-		'posts': paginated_queryset,
-		'page': page_request_var,
-	}
 
-	return render(request, 'blog/post_list.html', context)
-
-def Authors(request):
-	return render(request, 'blog/Authors.html')
-
-def post_detail(request, **kwargs):
-	post = get_object_or_404(Post, slug=kwargs['slug'])
-	context = {'post': post}
-	return render(request, 'blog/post_detail.html', context)
+class AuthorListView(ListView):
+	model = User
+	template_name = 'blog/Authors.html'
