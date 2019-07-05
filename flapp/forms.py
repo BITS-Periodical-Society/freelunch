f790b03769c568948c76b7b7f1fa0eb9e38acff7
ighosh98 <indraneel.ghosh1998@gmail.com>
2019-07-05 14:05:45 +0530

    Remove code redundancy

--- flapp/forms.py
+++ flapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post, Comment
+from .models import Post, Comment, Subscriber
 from pagedown.widgets import PagedownWidget
 
 
@@ -34,4 +34,14 @@ class CommentForm(forms.ModelForm):
 		super().__init__(*args, **kwargs)
 		self.fields['author'].widget.attrs.update({'class': 'form-control'})
 		self.fields['text'].widget.attrs.update({'class': 'form-control'})
-  
+
+class SubscribeForm(forms.ModelForm):
+
+	class Meta:
+		model = Subscriber
+		fields = '__all__'
+
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['name'].widget.attrs.update({'class': 'form-control'})
+		self.fields['email'].widget.attrs.update({'class': 'form-control'})
--- flapp/urls.py
+++ flapp/urls.py
@@ -12,8 +12,10 @@ urlpatterns = [
 	path('editor/<slug:slug>/', views.EdiorView.as_view(), name='editor_info'),
 	path('developer/<slug:slug>/', views.DeveloperView.as_view(), name='developer_info'),
 	path('writer/<slug:slug>/', views.WriterView.as_view(), name='writer_info'),
+	path('founder/<slug:slug>/', views.FounderView.as_view(), name='founder_info'),
 	path('section/<slug:section>/', views.PostListView.as_view(), name="section-post" ),
 	path('create/', views.PostCreateView.as_view(), name='post_create'),
+	path('subscribe/', views.SubscribeView, name='subscribe_form'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
