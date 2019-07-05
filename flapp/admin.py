a0da51d8f6c65fcbb2723a75cdd0fbc097b35493
Ak-shay <mittalakshay2000@gmail.com>
2019-07-05 22:54:09 +0530

    tag model added

--- flapp/admin.py
+++ flapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Writer, Editor, Developer, Subscriber, Founder, Comment
+from .models import Post, Writer, Editor, Developer, Subscriber, Founder, Comment, Tag
 from .forms import PostForm
 
 
@@ -10,3 +10,4 @@ admin.site.register(Editor)
 admin.site.register(Developer)
 admin.site.register(Subscriber)
 admin.site.register(Comment)
+admin.site.register(Tag)
