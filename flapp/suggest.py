2514221e3cf7621d6f30e96be5e5eedf870284f6
Ak-shay <mittalakshay2000@gmail.com>
2019-07-10 13:49:37 +0530

    recommendation error fixed

--- flapp/suggest.py
+++ flapp/suggest.py
@@ -13,22 +13,22 @@ def suggest_by_section(post):
 	suggestions = Post.objects.filter(section=section).exclude(title=post.title)
 	return suggestions
 
-def suggest_by_author(post):
-	"""LEVEL 2 SEARCH"""
-	authors = post.author.all()
-	suggestions = Post.objects.filter(author__in=authors).exclude(title=post.title)
-	return suggestions
-
 def suggest_by_editor(post):
-	"""LEVEL 3 SEARCH"""
+	"""LEVEL 2 SEARCH"""
 	editor = post.post_editor
 	suggestions = Post.objects.filter(post_editor=editor).exclude(title=post.title)
 	return suggestions
 
+def suggest_by_author(post):
+	"""LEVEL 3 SEARCH"""
+	authors = post.author.all()
+	suggestions = Post.objects.filter(author__in=authors).exclude(title=post.title)
+	return suggestions
+
 def recommend(post):
 	__0 = suggest_by_tag(post)
-	__1 = suggest_by_tag(post)
-	__3 = suggest_by_section(post)
+	__1 = suggest_by_section(post)
+	__2 = suggest_by_editor(post)
 	__3 = suggest_by_author(post)
 	__4 = Post.objects.all().exclude(title=post.title)
 	result = list(chain(__0, __1, __2, __3, __4))
