51d85663c3791359fc6cd385d7b4c5a67e4a0059
Ak-shay <mittalakshay2000@gmail.com>
2019-07-10 13:57:44 +0530

    editor removed

--- flapp/suggest.py
+++ flapp/suggest.py
@@ -13,14 +13,8 @@ def suggest_by_section(post):
 	suggestions = Post.objects.filter(section=section).exclude(title=post.title)
 	return suggestions
 
-def suggest_by_editor(post):
-	"""LEVEL 2 SEARCH"""
-	editor = post.post_editor
-	suggestions = Post.objects.filter(post_editor=editor).exclude(title=post.title)
-	return suggestions
-
 def suggest_by_author(post):
-	"""LEVEL 3 SEARCH"""
+	"""LEVEL 2 SEARCH"""
 	authors = post.author.all()
 	suggestions = Post.objects.filter(author__in=authors).exclude(title=post.title)
 	return suggestions
@@ -28,10 +22,9 @@ def suggest_by_author(post):
 def recommend(post):
 	__0 = suggest_by_tag(post)
 	__1 = suggest_by_section(post)
-	__2 = suggest_by_editor(post)
-	__3 = suggest_by_author(post)
-	__4 = Post.objects.all().exclude(title=post.title)
-	result = list(chain(__0, __1, __2, __3, __4))
+	__2 = suggest_by_author(post)
+	__3 = Post.objects.all().exclude(title=post.title)
+	result = list(chain(__0, __1, __2, __3))
 	seen = set()
 	result =  [x for x in result if not (x in seen or seen.add(x))]
 	return result[:6]
