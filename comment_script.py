e48c610bd7b46bae97dc48a7e1b645f754bba17a
ighosh98 <indraneel.ghosh1998@gmail.com>
2019-07-27 11:08:47 +0530

    Comment Script

---
+++ comment_script.py
@@ -0,0 +1,7 @@
+def line_pre_adder(filename, line_to_prepend):
+    f = fileinput.input(filename, inplace=1)
+    for xline in f:
+        if f.isfirstline():
+            print line_to_prepend.rstrip('\r\n') + '\n' + xline,
+        else:
+            print (xline)
